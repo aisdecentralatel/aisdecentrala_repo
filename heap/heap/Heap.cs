48f7d3a0ae2083b2d8577726a1dbba066a0b8fd1
jarekb123 <jarekbutek@gmail.com>
2015-10-15 23:29:54 +0200

    commit

---
+++ heap/heap/Heap.cs
@@ -0,0 +1,38 @@
+﻿namespace heapsort
+{
+    class Heap
+    {
+        static public int[] CreateFromArray(int[] array)
+        // Zwraca tablice zamieniona na kopiec
+        {
+            for (var i = 0; i < array.Length - 1; i++)
+            {
+                var index = i + 1;
+
+                while (array[index / 2] > array[index] && index >= 1)
+                {
+                    var tmp = array[index / 2];
+                    array[index / 2] = array[index];
+                    array[index] = tmp;
+                    index /= 2;
+                }
+            }
+            return array;
+        }
+        static public int[] HeapSort(int[] array)
+        // Sortuje utworzony kopiec
+        {
+            var sorted = false;
+            var index = array.Length - 1;
+            while (!sorted)
+            {
+                var first = array[0];
+                var last = array[array.Length - 1];
+
+            }
+
+
+            return array;
+        }
+    }
+}
---
+++ heap/heap/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace heapsort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long n = 0;
+            Console.Write("Podaj ilosc liczb: ");
+            n = int.Parse(Console.ReadLine());
+            int[] tab = new int[n];
+            Random r = new Random(DateTime.Now.Millisecond);
+            for (int i = 0; i < n; i++)
+            {
+
+                tab[i] = r.Next(0, 1000);
+
+
+                //                Console.Write("{0} ", tab[i]);
+            }
+            Console.WriteLine("\nTworzenie kopca :)");
+            tab = Heap.CreateFromArray(tab);
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("{0} ", tab[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
